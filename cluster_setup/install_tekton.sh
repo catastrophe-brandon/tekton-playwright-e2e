8b7f50824d1383d6f3db4f38c48e3cf64a797bc6
Darth Tweed <btweed@redhat.com>
2025-12-03 15:14:31 -0600

    Make tekton install script wait until tekton is operational

--- cluster_setup/install_tekton.sh
+++ cluster_setup/install_tekton.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 # Install Tekton and required tasks to execute the pipeline
-# Requires tkn cli and kubectl 
+# Requires tkn cli and kubectl
 set -e
 
 # Install Tekton to an existing minikube instance
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 
-# This will show `1/1` under the READY column once tekton is available
-# kubectl get pods --namespace tekton-pipelines --watch
+echo "Waiting for Tekton controller to be ready..."
+kubectl wait --for=condition=ready pod -l app=tekton-pipelines-controller -n tekton-pipelines --timeout=120s
+
+echo "Waiting for Tekton webhook to be ready..."
+kubectl wait --for=condition=ready pod -l app=tekton-pipelines-webhook -n tekton-pipelines --timeout=120s
+
+echo "Tekton installation complete and ready!"
 
 # Install the git-clone task for use in the pipeline
 tkn hub install task git-clone
