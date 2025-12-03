3523e6e8444ae43c4927754f8b40eaadbf5921f1
Darth Tweed <btweed@redhat.com>
2025-12-03 15:33:46 -0600

    Updates for startup to prevent DNS issues

--- cluster_setup/start.sh
+++ cluster_setup/start.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
+# Only the pipeline needs these env vars. If they are set when we start minikube they can cause DNS resolution issues.
+unset HTTP_PROXY
+unset HTTPS_PROXY
+
+
 # check status and start minikube if it's not running
 # More disk space needed for running e2e tests, Tekton, etc.
 minikube start --driver=podman --container-runtime=cri-o --disk-size=40g
